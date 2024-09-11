4eadcd5fb9f9c5e084e083c3d10ad2ceedafe1e6
doGior <43815981+doGior@users.noreply.github.com>
2024-09-11 18:23:38 +0200

    First commit

---
+++ app/src/main/java/it/dogior/ncc/MainActivity.kt
@@ -0,0 +1,31 @@
+package it.dogior.ncc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import it.dogior.ncc.presentation.HomeScreen
+import it.dogior.ncc.ui.theme.NomiCoseCittàTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NomiCoseCittàTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HomeScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
